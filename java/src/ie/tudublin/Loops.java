1dfb39a06dc0af61a1b04bb89e7802e956d948c6
Wirexia-git <d20124630@mytudublin.ie>
2022-02-14 12:05:36 +0000

    for loops X shape works right

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -48,6 +48,21 @@ public class Loops extends PApplet
 				//color(0, 128, 0);
 				break;
 			case 1:
+				background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width / (float) squares;
+				for(int i = 0 ; i < squares ; i++)
+				{
+					noStroke();
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					float x = map(i, 0, squares, 0, width);
+					rect(x, x, h, h);
+					rect((width - h) - x, x, h, h);
+				}
+				//map(a,b,c,d,e);
+				//a = inputvalue
+				//b - c -- start and end of the first range
+				// d, e 0 -- start and end of the end range
 				//line(10, 10, 250, 150);
 				//color(255, 0, 0);
 				break;
